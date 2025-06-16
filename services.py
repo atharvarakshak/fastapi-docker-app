c779ae3191103bd7b075fb1ff5fe4f54b3d4a921
atharvarakshak <atharvarakshak23@gmail.com>
2025-06-16 10:23:56 +0000

    create contact route

---
+++ services.py
@@ -0,0 +1,30 @@
+import database as _database
+import models as _models
+import schemas as _schemas
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+
+def _add_tables():
+    return _database.Base.metadata.create_all(bind=_database.engine)
+
+
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+async def create_contact(
+    contact: _schemas.CreateContact, db="Session"
+) -> _schemas.Contact:
+    contact = _models.Contact(**contact.model_dump())
+    db.add(contact)
+    db.commit()
+    db.refresh(contact)
+
+    return _schemas.Contact.model_validate(contact)
